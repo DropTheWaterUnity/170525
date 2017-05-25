81a7ff2f2128fd8cfe8c1cfd7024be1859582146
0220jjhh <0220jjhh@daum.net>
2017-05-25 11:39:12 +0900

    170525
    Compile error

---
+++ Drop_The_Water_02/Assets/Resources/Scripts/MainPage/Action.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Instruction
+{
+	public class Action : Instruction	//for move, jump, break
+	{
+		public Action (INSTRUCTION instruction)
+		{
+			if(instruction == INSTRUCTION.PLUS || instruction == INSTRUCTION.MINUS || instruction == INSTRUCTION.PLAY)
+				this.instruction = instruction;
+		}
+
+		public Action(String instruction)
+		{
+			this.instruction = convert (instruction);
+		}
+
+
+		public override bool nextValid ()
+		{
+			return next is Direction;
+		} 
+	}
+}
